9b202ed9cbf3444cbe85f322ce3906720fd9f4a7
Seb Pollard <seb@spookmedia.com>
2010-01-20 19:29:49 +0000

    Added readme file for libs collection, plus added some extra documentation to the AutoLinker class.

---
+++ README.markdown
@@ -0,0 +1,8 @@
+SP-Libs
+=======
+
+A collection of small classes that are often useful.
+
+Currently consists of:
+
+* AutoLinker - Processes text strings automatically putting `<a>` tags around urls and email addresses.
--- autolinker.php
+++ autolinker.php
@@ -1,26 +1,43 @@
 <?php
 
+/**
+ * AutoLinker class automatically converts inline urls and email address in strings into 
+ * clickable links.
+ *
+ * @author Seb Pollard <seb@spolster.co.uk>
+ * @version 1
+ */
 class AutoLinker {
 
+	/**
+	 * Converts all urls and emails to links.
+	 * @param 	string 	$string		The text (possibly) containing emails/urls.
+	 * @return 	string				The processed text.
+	 */
 	static public function linkAll($string) {
 		$string = self::linkEmails($string);
 		return self::linkURLS($string);
 	}
 
+	/**
+	 * Wraps all urls in passed text in a tags.
+	 * @param 	string 	$string		The text (possibly) containing urls.
+	 * @return 	string				The processed text.
+	 */
 	static public function linkURLs($string) {
 		return preg_replace_callback(
 			"/
-				([\s]|^)												# word boundary or start of string.
-				(\()?													# (optional) opening parenthesis.
-				(https?:\/\/)?											# (optional) protocol
-				(														# begin domain name
-					([A-Z0-9]([A-Z0-9\-]{0,61}[A-Z0-9])?\.)+			# One or more subdomains 
-																		# (1-63 chars per rfc1035, plus trailing dot)
-					[A-Z]{2,6}											# TLD
-				)														# end domain name
-				(\/[A-Z0-9\/\.\-\_\?\=\&\+\%\#]+)?						# path
-				(\))?													# (optional) closing parenthesis.
-				(\s|$)													# word boundary or end of string
+				([\s]|^)										# word boundary or start of string.
+				(\()?											# (optional) opening parenthesis.
+				(https?:\/\/)?									# (optional) protocol
+				(												# begin domain name
+					([A-Z0-9]([A-Z0-9\-]{0,61}[A-Z0-9])?\.)+	# One or more subdomains 
+																# (1-63 chars per rfc1035, plus trailing dot)
+					[A-Z]{2,6}									# top-level domain (2-6 chars)
+				)												# end domain name
+				(\/[A-Z0-9\/\.\-\_\?\=\&\+\%\#]+)?				# path
+				(\))?											# (optional) closing parenthesis.
+				(\s|$)											# word boundary or end of string
 			/ix",
 			array('AutoLinker', "_linkUrlsCallback"), $string);
 	}
@@ -47,6 +64,11 @@ class AutoLinker {
 		return $pre . '<a href="' . $url . '">' . $url . '</a>' . $post;
 	}
 
+	/**
+	 * Wraps all email address in passed text in a tags (mailto:).
+	 * @param 	string 	$string		The text (possibly) containing email addresses.
+	 * @return 	string				The processed text.
+	 */
 	static public function linkEmails($string) {
 		return preg_replace("/\b([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})\b/i", "<a href='mailto:\\1'>\\1</a>", $string);
 	}
